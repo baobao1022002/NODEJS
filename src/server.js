31cb60566fa34ace7fea80c3296665c8a7060ccf
qbao10202 <quocbao01652@gmail.com>
2024-09-05 08:37:02 +0700

    connectdb_render_raw_data

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import connectDB from "./config/connectdb";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
@@ -8,8 +8,10 @@ require("dotenv").config();
 let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 viewEngine(app);
 initWebRoutes(app);
+connectDB();
 
 let port = process.env.PORT || 6969;
 app.listen(port, () => {
